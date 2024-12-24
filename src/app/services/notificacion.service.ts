9dce32bceabb993f2e3e9e45451545aaa2d9fe02
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2024-12-23 23:10:40 -0300

    Se agrega lib para notific

---
+++ src/app/services/notificacion.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import Toastify from 'toastify-js'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacionService {
+
+  constructor() { }
+
+  mensajeDeError(err: any) {
+    let msg="Error verifique con administración."
+    if(err?.status !== 0) {
+      msg = err?.error?.message
+    } else {
+      msg="No hay respuesta del servidor."
+    }
+
+    Toastify({
+      text: msg,
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #f50000, #ff5b14)",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
+  }
+}
